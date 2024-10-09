a3df0beba2748f01b874b824d1f33f9680c3920a
Carl Hillman <carl.hillman@gmail.com>
2024-10-09 21:05:35 +0200

    addeed vercel deplo

--- react-vercel-test/src/App.js
+++ react-vercel-test/src/App.js
@@ -7,9 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
-
-          wow funkar äntligen!
+          wow VERCEL funkar äntligen!
         </p>
         <a
           className="App-link"
